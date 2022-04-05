a248b817e0bc3eae6cdfed857db175c4c99923aa
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-04-05 09:11:14 +0200

    Add header / sidebar to DOM

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import { addDays, startOfDay, format } from 'date-fns';
+import { header, sidebar, buildDom } from './pages/dom';
+
+buildDom(header, sidebar);
 
 console.log('Locked \n \n(⊙.⊙(☉̃ₒ☉)⊙.⊙) \n \n & Loaded ');
---
+++ src/pages/dom.js
@@ -0,0 +1,48 @@
+import { elFactory } from "../functions/global-functions";
+import menuIcon from '../assets/icons/menu.svg';
+import titleIcon from '../assets/icons/task-icon.svg';
+import allTasksIcon from '../assets/icons/all-tasks.svg';
+import todayIcon from '../assets/icons/today-icon.svg';
+import weekIcon from '../assets/icons/week-icon.svg';
+import priorityIcon from '../assets/icons/priority-high.svg';
+
+// add header
+export const header = elFactory('HEADER', { id: 'myHeader' }, document.body,
+  elFactory('img', { src: menuIcon }, ''),
+  elFactory('div', { id: 'iconTitle' }, '',
+    elFactory('img', { src: titleIcon }, ''),
+    elFactory('h1', '', '', "Todo List")
+  )
+);
+
+// Add sidebar
+export const sidebar = elFactory('div', { id: 'sidebar' }, document.body,
+  elFactory('h2', { class: 'sideTitle' }, '', 'Quick View'),
+  elFactory('hr', '', ''),
+  elFactory('div', { class: "sideNav" }, '',
+    elFactory('img', { src: allTasksIcon }, ''),
+    elFactory('div', '', '', 'All Tasks')
+  ),
+  elFactory('div', { class: "sideNav" }, '',
+    elFactory('img', { src: todayIcon }, ''),
+    elFactory('div', '', '', 'Today')
+  ),
+  elFactory('div', { class: "sideNav" }, '',
+    elFactory('img', { src: weekIcon }, ''),
+    elFactory('div', '', '', 'Next 7 days')
+  ),
+  elFactory('div', { class: "sideNav" }, '',
+    elFactory('img', { src: priorityIcon }, ''),
+    elFactory('div', '', '', 'High Priority'),
+    elFactory('h2', { class: 'sideTitle' }, '', 'Projects'),
+    elFactory('hr', '', ''),
+  )
+);
+
+export function buildDom(...domElements) {
+  domElements.forEach(element => {
+    return element;
+  });
+};
+
+
