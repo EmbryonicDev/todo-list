663aa3cec43867cc680066a07ea60a7ff85af9bf
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-04-05 16:39:11 +0200

    Import form.css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import { addDays, startOfDay, format } from 'date-fns';
 import { header, sidebar, buildDom } from './pages/dom';
 import '../src/styles/modern-normalize.css';
-import '../src/styles/style.css'
+import '../src/styles/style.css';
+import '../src/styles/form.css';
+import { getTaskForm } from './pages/new-task.js';
 
 buildDom(header, sidebar);
+
+getTaskForm();
 
 console.log('Locked \n \n(⊙.⊙(☉̃ₒ☉)⊙.⊙) \n \n & Loaded ');
