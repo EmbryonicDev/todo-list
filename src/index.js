500b3949dc7d80749ee02bef167070928e62751e
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-04-05 10:44:30 +0200

    Import styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { addDays, startOfDay, format } from 'date-fns';
 import { header, sidebar, buildDom } from './pages/dom';
+import '../src/styles/modern-normalize.css';
+import '../src/styles/style.css'
 
 buildDom(header, sidebar);
 
