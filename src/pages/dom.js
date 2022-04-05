689ea913a2cd4a417c630dcdb6d1ce628ca49a0e
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-04-05 16:40:52 +0200

    Remove redundant const

--- src/pages/dom.js
+++ src/pages/dom.js
@@ -54,7 +54,7 @@ export const sidebar = elFactory('div', { id: 'sidebar' }, document.body,
 
 // add task form 
 export function addTaskForm() {
-  const newTaskForm = elFactory('form', {id: 'myForm'}, document.body,
+  elFactory('form', {id: 'myForm'}, document.body,
     elFactory('p', '', '',
       elFactory('label', { for: 'dateAdded' }, '', 'Date Added:'),
       elFactory('input', { type: 'date', id: 'dateAdded', value: new Date().toISOString().slice(0, 10) }, '')
