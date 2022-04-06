d5306aaf46b8c0f48e352394f923a13f50a917ad
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-04-06 16:34:53 +0200

    Add empty tasksArr & parse to JASON. Add taskFactory

--- src/pages/tasks.js
+++ src/pages/tasks.js
@@ -1,7 +1,21 @@
 import { addTaskForm } from "./dom";
 
+let tasksArr = JSON.parse(localStorage.getItem("tasksArr")) || [];
+
 export const getTaskForm = () => {
   document.getElementById('addTaskBtn').onclick = (e) => {
     if(!document.getElementById('taskForm') && !document.getElementById('projectForm')) addTaskForm();
   }
 }
+
+const taskFactory = (dateAdded, taskName, description, dueDate, project, priority, notes) => {
+  return {
+    taskName: taskName,
+    dateAdded: dateAdded,
+    description: description,
+    dueDate: dueDate,
+    project: project,
+    priority: priority,
+    notes: notes,
+  }
+};
