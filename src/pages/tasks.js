7ea035027b8e44429cd0425577a3b5185cb6a8ce
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-04-06 19:25:24 +0200

    Move taskFactory to bottom

--- src/pages/tasks.js
+++ src/pages/tasks.js
@@ -10,18 +10,6 @@ export const getTaskForm = () => {
   }
 }
 
-const taskFactory = (dateAdded, taskName, description, dueDate, project, priority, notes) => {
-  return {
-    taskName: taskName,
-    dateAdded: dateAdded,
-    description: description,
-    dueDate: dueDate,
-    project: project,
-    priority: priority,
-    notes: notes,
-  }
-};
-
 const submitTaskBtn = () => {
   document.getElementById('taskSubmit').onclick = (e) => {
     e.preventDefault();
@@ -45,3 +33,15 @@ const removeTasksForm = () => {
   const taskForm = document.getElementById('taskForm');
   taskForm.parentElement.removeChild(taskForm);
 }
+
+const taskFactory = (dateAdded, taskName, description, dueDate, project, priority, notes) => {
+  return {
+    taskName: taskName,
+    dateAdded: dateAdded,
+    description: description,
+    dueDate: dueDate,
+    project: project,
+    priority: priority,
+    notes: notes,
+  }
+};
