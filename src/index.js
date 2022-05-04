68f268d2dba2005eb12a702262232fa9a56e4f69
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-05-04 12:18:38 +0200

    Create addStartupProjects()

--- src/index.js
+++ src/index.js
@@ -5,14 +5,16 @@ import '../src/styles/changed-style.css';
 import '../src/styles/night-mode.css';
 import '../src/styles/forms.css';
 import { pageStyle, projects, tasks } from './pages/script.js';
-import { buildDom } from './functions/global-functions';
+import { addStartupProjects, buildDom } from './functions/global-functions';
 
 console.log('Locked \n \n(⊙.⊙(☉̃ₒ☉)⊙.⊙) \n \n & Loaded ');
 
 buildDom(header(), sidebar(), activeTitleDisplay());
 
+addStartupProjects.init();
+
 projects.init();
 
-tasks.init();
-
 pageStyle.init();
+
+tasks.init();
